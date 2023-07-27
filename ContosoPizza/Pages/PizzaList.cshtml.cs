9eed25c96654c8b24b4c48d57c60a2053a7e9fee
ALEX OLIVEIRA DA SILVA <alex.osilva7@senacsp.edu.br>
2023-07-27 17:22:10 -0300

    Pizzaalex

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -2,11 +2,15 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ContosoPizza.Models;
 using ContosoPizza.Services;
+using System.Security.Policy;
 
 namespace ContosoPizza.Pages
 {
     public class PizzaListModel : PageModel
     {
+        [BindProperty]
+
+        public Pizza NewPizza { get ; set; } = default!;
         private readonly PizzaService _service;
         public IList<Pizza> PizzaList { get; set; } = default!;
 
@@ -18,6 +22,24 @@ namespace ContosoPizza.Pages
         public void OnGet()
         {
             PizzaList = _service.GetPizzas();
+        }
+
+        public IActionResult OnPost ()
+        {
+            if (!ModelState.IsValid || NewPizza == null)
+            {
+                return Page();
+            }
+
+            _service.AddPizza(NewPizza);
+            return RedirectToAction("Get");
+        }
+        public IActionResult OnPostDelete(int id)
+        {
+            _service.DeletePizza(id);
+
+            return RedirectToAction("Get");
+
         }
     }
 }
